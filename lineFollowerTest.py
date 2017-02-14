ed21a74e04c13455afd7f004ca981367cabaedbc
Brian Corteil <brian.corteil@gmail.com>
2017-02-14 00:51:57 +0000

    new library & test example
    new library created and test program

---
+++ lineFollowerTest.py
@@ -0,0 +1,9 @@
+import lineSensor
+import time
+
+line = lineSensor.LineSensor(9, 10, 11)
+
+while True:
+    read_line = line.read()
+    print read_line
+    time.sleep(1)
---
+++ lineSensor.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+
+class LineSensor:
+    def __init__(self, left, middle, right):
+        self._left = left
+        self._middle = middle
+        self._right = right
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup([left, middle, right], GPIO.IN)
+
+    def read(self):
+        left_sensor, middle_sensor, right_sensor = 0, 0, 0
+        if GPIO.input(self._left) is True:
+            left_sensor = 1
+        if GPIO.input(self._middle) is True:
+            middle_sensor = 1
+        if GPIO.input(self._right) is True:
+            right_sensor = 1
+        values = {"left": left_sensor, "middle": middle_sensor, "right": right_sensor}
+        return values
